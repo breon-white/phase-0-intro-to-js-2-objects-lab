f130b9eb8234e41b1d35ade640eda0133770c671
Breon White <bdwhite92@gmail.com>
2021-12-12 16:20:55 -0500

    commit tree 5f05d4ccf403872591b5ea1273096659dc4af893

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function deleteFromEmployeeByKey(employee, key) {
 }
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
-    return employee;
+    return employee; 
 }
